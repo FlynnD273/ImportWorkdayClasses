c208cb80876ea07a78df18f5992ca48c39e47c35
Flynn Duniho <flynn@harveyosity.org>
2023-07-11 00:13:35 -0700

    Revert changes and add a description to the alarm

--- exportwd.py
+++ exportwd.py
@@ -1,6 +1,7 @@
 from icalendar import Calendar, Event, Alarm
-import datetime as dt
-import arrow
+import pytz
+import datetime
+from datetime import datetime as dt
 import pandas as pd
 import sys
 import uuid
@@ -8,14 +9,10 @@ import uuid
 warning = 15 # How many minutes before class to show reminder
 timezone = 'US/Eastern' # The time zone to use for the calendar events
 
-def to_dt (arr: arrow.Arrow) -> dt.datetime:
-    return dt.datetime(arr.year, arr.month, arr.day, arr.hour, arr.minute, arr.second, tzinfo= arr.tzinfo)
-
 def parse_event (row):
-    now = arrow.now()
     event = Event()
     event['uid'] = str(uuid.uuid4())
-    event.add('dtstamp', to_dt(arrow.now()))
+    event.add('dtstamp', dt.now())
 
     event.add('summary', row.course_listing + ' | ' + row.instructional_format)
     event.add('description', row.delivery_mode)
@@ -24,18 +21,11 @@ def parse_event (row):
     event.add('location', meeting_pat[2])
 
     times = meeting_pat[1].split(' - ')
-    start = dt.datetime.strptime(times[0].strip(), "%I:%M %p")
-    start = arrow.get(start, timezone)
-    start = start.replace(year= now.year, month= now.month, day= now.day)
+    start = dt.strptime(times[0].strip(), "%I:%M %p")
+    start.replace(tzinfo=pytz.timezone(timezone))
 
-    # Convert to UTC time 
-    # Some programs don't take into account timezones properly
-    start = start.to("UTC")
-
-    end = dt.datetime.strptime(times[1].strip(), "%I:%M %p")
-    end = arrow.get(end, timezone)
-    end = end.replace(year= now.year, month= now.month, day= now.day)
-    end = end.to("UTC")
+    end = dt.strptime(times[1].strip(), "%I:%M %p")
+    end.replace(tzinfo=pytz.timezone(timezone))
 
     workdayToIcal = {'M': 'mo', 'T': 'tu', 'W': 'we', 'R': 'th', 'F': 'fr'}
     weekdays = list(workdayToIcal.values())
@@ -44,27 +34,26 @@ def parse_event (row):
         if letter in meeting_pat[0]:
             days.append(workdayToIcal[letter])
 
-    startDate = arrow.get(row.start_date)
+    startDate = row.start_date.date()
     while startDate.weekday() >= len(weekdays) or weekdays[startDate.weekday()] not in days:
-        startDate = startDate.shift(days=1)
-
-    event.add('dtstart', to_dt(startDate.replace(hour=start.hour, minute=start.minute)))
-    event.add('dtend', to_dt(startDate.replace(hour=end.hour, minute=end.minute)))
-    event.add('rrule', {'freq': 'weekly', 'until': to_dt(arrow.get(row.end_date).shift(days=1)), 'byday': days})
-
-    alarm = Alarm()
-    alarm.add('action', 'DISPLAY')
-    alarm.add('trigger', dt.timedelta(minutes= warning))
-    event.add_component(alarm)
-
-    eventDays = str.join(", ", event['rrule']['BYDAY'])
+        startDate = datetime.date(startDate.year, startDate.month, startDate.day + 1)
 
     print(event['summary'])
     print(f"Given time: {meeting_pat[1].strip()}")
-    # print(f"Parsed time: {start} - {end}")
-    print(f"Parsed time: {start.to(timezone):hh:mm A} - {end.to(timezone):hh:mm A}")
+    print(f"Parsed time: {start:%I:%M %p} - {end:%I:%M %p}")
 
-    print(f"Calendar event:\nSummary: {event['summary']}\nLocation: {event['location']}\nRepeats on {eventDays} until {event['rrule']['UNTIL'] - dt.timedelta(days=1):%A, %B %d %y}\nReminder {warning} minutes before the event")
+    event.add('dtstart', dt.combine(startDate, start.timetz()))
+    event.add('dtend', dt.combine(startDate, end.timetz()))
+    event.add('rrule', {'freq': 'weekly', 'until': row.end_date.date() + datetime.timedelta(days=1), 'byday': days})
+
+    alarm = Alarm()
+    alarm.add('action', 'DISPLAY')
+    alarm.add('trigger', datetime.timedelta(minutes = warning))
+    alarm.add('description', "display a notification for the event")
+    event.add_component(alarm)
+
+    eventDays = str.join(", ", event['rrule']['BYDAY'])
+    print(f"Calendar event:\nSummary: {event['summary']}\nLocation: {event['location']}\nRepeats on {eventDays} until {event['rrule']['UNTIL'] - datetime.timedelta(days=1):%A, %B %d %y}\nReminder {warning} minutes before the event")
     print()
     return event
 
